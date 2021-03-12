f3340e2bbff189487dab4002605c27fd17a89581
astr0n0mer <imran2000786@gmail.com>
2021-03-12 18:28:39 +0530

    Completed interfaces-excercise

---
+++ Interfaces-ex/src/Main.java
@@ -0,0 +1,62 @@
+// An interface is a completely "abstract class" that is used to group related methods with empty bodies
+// Understand interface as a list of compulsory methods(along with their signatures) to be defined when you implement a
+// given interface
+// Interface methods are by default abstract and public
+// Interface attributes are by default public, static and final
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Player john = new Player("John", 100, 15);
+        saveObject(john);
+        System.out.println(john);
+
+        john.setWeapon("Sword");
+        saveObject(john);
+        System.out.println(john);
+
+        loadObject(john);
+        saveObject(john);
+        System.out.println(john);
+    }
+
+    public static ArrayList<String> readValues() {
+        ArrayList<String> values = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        int index = 0;
+        System.out.println("Choose\n" +
+                "1 to enter a string\n" +
+                "0 to quit");
+        while (!quit) {
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+                case 1:
+                    System.out.print("Enter a string: ");
+                    String stringInput = scanner.nextLine();
+                    values.add(index, stringInput);
+                    index++;
+                    break;
+            }
+        }
+        return values;
+    }
+
+    public static void saveObject(ISavable objectToSave) {
+        for (int i = 0; i < objectToSave.write().size(); i++) {
+            System.out.println("Saving " + objectToSave.write().get(i) + " to file.");
+        }
+    }
+
+    public static void loadObject(ISavable objectToLoad) {
+        ArrayList<String> values = readValues();
+        objectToLoad.read(values);
+    }
+}
