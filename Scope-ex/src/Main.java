4ee2ebeef5f7c8dd8c2f33626cf04424a05f8d47
astr0n0mer <imran2000786@gmail.com>
2021-03-27 20:19:02 +0530

    Completed Scope-exercise

---
+++ Scope-ex/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    private static final Scanner x = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.print("Enter an integer: ");
+        X x = new X(Main.x.nextInt());
+        x.x(); // calling a method from class 'X'
+    }
+}
